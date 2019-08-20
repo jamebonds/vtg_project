38a54aaa4569863446c1fcc1f167c23e5298a922
Jamebond <tanakorn.niamchaona@gmail.com>
2019-08-20 13:20:31 +0700

    update

--- cut/writeFilecsv.py
+++ cut/writeFilecsv.py
@@ -1,13 +1,17 @@
 import os
 import csv
-value = 20.31
-mt = 'MT'
-yd =  'YD'
-with  open('csvtest.csv','w',newline='') as csvfile:
-    header =  ['YD','MT']
-    writer = csv.DictWriter(csvfile,fieldnames=header)
-    writer.writeheader()
-    writer.writerow({yd:value,mt:value})
+# value = 20.31
+# mt = 'MT'
+# yd =  'YD'
+if(os.path.ismount('/media/AIP') == True):
+    print('!!!!')
+else :
+    print('????')
+# with  open('csvtest.csv','w',newline='') as csvfile:
+#     header =  ['YD','MT']
+#     writer = csv.DictWriter(csvfile,fieldnames=header)
+#     writer.writeheader()
+#     writer.writerow({yd:value,mt:value})
     # writer.writerow({mt: 'Baked', yd: 'Beans'})
 # writer.writerow({'MT':'WTF','YD':'WTF'})
 
