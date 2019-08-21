4e23403e5461fa14914d4d180112d4a6e4efbff0
Jamebond <tanakorn.niamchaona@gmail.com>
2019-08-21 17:02:56 +0700

    update

--- setup.py
+++ setup.py
@@ -5,17 +5,11 @@ print('setup program')
 
 def setup():
     try:
-        print('1 :Setup cifs,pandas,chmod')
-        print('2 :create Directory AIP ')
-        print('input value :')
-        x = input()
-        if (x == '1'):
-            os.system('sudo apt-get update -y')
-            os.system('sudo apt-get install cifs-utils -y')
-            os.system('sudo apt-get install python-pandas -y && sudo apt-get install python3-pandas -y')
-            os.system('sudo chmod 775 cut/* && sudo chmod 775 spd/*')
-        elif(x=='2'):
-            os.system('sudo mkdir /media/AIP')
+        os.system('sudo apt-get update -y')
+        os.system('sudo apt-get install cifs-utils -y')
+        os.system('sudo apt-get install python-pandas -y && sudo apt-get install python3-pandas -y')
+        os.system('sudo chmod 775 cut/* && sudo chmod 775 spd/*')
+        os.system('sudo mkdir /media/AIP')
     except IOError:
         print('Error :' + IOError)
     finally:
