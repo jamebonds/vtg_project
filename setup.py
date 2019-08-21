510f5fcea098a4536ffd4af0b64192f4c8398c40
Jamebond <tanakorn.niamchaona@gmail.com>
2019-08-21 16:23:54 +0700

    update

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ try:
     os.system('sudo apt-get update -y')
     os.system('sudo apt-get install cifs-utils -y')
     os.system('sudo apt-get install python-pandas -y && sudo apt-get install python3-pandas -y')
+    os.system('sudo chmod 775 vtg_project/cut/* && sudo chmod 775 vtg_project/spd/*')
 except IOError :
     print('Error :' + IOError)
 finally:
